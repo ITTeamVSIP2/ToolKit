7354955f47f3a578d141705c74cbc6738e9d7001
Tâm Minh <minhtam030797@gmail.com>
2022-12-02 14:19:07 +0700

    alo 1234 s

--- SupportTools/Main.Designer.cs
+++ SupportTools/Main.Designer.cs
@@ -58,8 +58,7 @@
             this.accordionControlElement5 = new DevExpress.XtraBars.Navigation.AccordionControlElement();
             this.documentManager = new DevExpress.XtraBars.Docking2010.DocumentManager(this.components);
             this.tabbedView = new DevExpress.XtraBars.Docking2010.Views.Tabbed.TabbedView(this.components);
-            this.accordionControlElement19 = new DevExpress.XtraBars.Navigation.AccordionControlElement();
-            this.accordionControlElement20 = new DevExpress.XtraBars.Navigation.AccordionControlElement();
+            this.accordionControlElement21 = new DevExpress.XtraBars.Navigation.AccordionControlElement();
             ((System.ComponentModel.ISupportInitialize)(this.dockManager)).BeginInit();
             this.dockPanel1.SuspendLayout();
             this.dockPanel1_Container.SuspendLayout();
@@ -125,8 +124,7 @@
             this.accordionControlElement13,
             this.accordionControlElement14,
             this.Other,
-            this.accordionControlElement19,
-            this.accordionControlElement20});
+            this.accordionControlElement21});
             this.accordionControl.Location = new System.Drawing.Point(0, 0);
             this.accordionControl.LookAndFeel.SkinMaskColor = System.Drawing.SystemColors.Menu;
             this.accordionControl.LookAndFeel.SkinMaskColor2 = System.Drawing.SystemColors.ButtonFace;
@@ -295,6 +293,7 @@
             // 
             this.Other.Elements.AddRange(new DevExpress.XtraBars.Navigation.AccordionControlElement[] {
             this.accordionControlElement5});
+            this.Other.Expanded = true;
             this.Other.Name = "Other";
             this.Other.Text = "Other";
             // 
@@ -318,17 +317,10 @@
             this.tabbedView.AppearancePage.HeaderActive.Options.UseBackColor = true;
             this.tabbedView.DocumentClosed += new DevExpress.XtraBars.Docking2010.Views.DocumentEventHandler(this.tabbedView_DocumentClosed);
             // 
-            // accordionControlElement19
+            // accordionControlElement21
             // 
-            this.accordionControlElement19.Name = "accordionControlElement19";
-            this.accordionControlElement19.Style = DevExpress.XtraBars.Navigation.ElementStyle.Item;
-            this.accordionControlElement19.Text = "Hello";
-            // 
-            // accordionControlElement20
-            // 
-            this.accordionControlElement20.Name = "accordionControlElement20";
-            this.accordionControlElement20.Style = DevExpress.XtraBars.Navigation.ElementStyle.Item;
-            this.accordionControlElement20.Text = "Element20";
+            this.accordionControlElement21.Name = "accordionControlElement21";
+            this.accordionControlElement21.Text = "ALOOOOOOOOOOO";
             // 
             // Main
             // 
@@ -387,8 +379,7 @@
         private DevExpress.XtraBars.Navigation.AccordionControlElement accordionControlElement16;
         private DevExpress.XtraBars.Navigation.AccordionControlElement accordionControlElement17;
         private DevExpress.XtraBars.Navigation.AccordionControlElement accordionControlElement18;
-        private DevExpress.XtraBars.Navigation.AccordionControlElement accordionControlElement19;
-        private DevExpress.XtraBars.Navigation.AccordionControlElement accordionControlElement20;
+        private DevExpress.XtraBars.Navigation.AccordionControlElement accordionControlElement21;
     }
 }
 
